aa824707a563cc10ec91ecee5a5fa284c1691324
Jacob The Losen <jwt325@lehigh.edu>
2024-04-30 15:12:19 -0400

    added dummy table to test access

--- amplify/backend/function/fredsmanager/src/index.py
+++ amplify/backend/function/fredsmanager/src/index.py
@@ -1,8 +1,13 @@
 import json
+import boto3
 
 def handler(event, context):
   print('received event:')
   print(event)
+
+  dynamodb = boto3.resource('dynamodb')
+  table = dynamodb.Table('firsttable')
+  response = table.scan()
   
   return {
       'statusCode': 200,
@@ -11,5 +16,5 @@ def handler(event, context):
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
       },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
+      'body': json.dumps('Hello from your new Amplify Python lambda! DynamoDB says:', response)
   }
