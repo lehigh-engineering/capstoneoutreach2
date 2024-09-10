0dccd50b69962ef3f28f21b094d7101568199752
Lara Flaten <laraflaten@Laras-MBP.cable.rcn.com>
2024-09-09 20:51:39 -0400

    Plz build

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import './HomePage.css';
 
-// import cipherImg from './images/cipher.jpg';
-// import hackerImg from './images/hacker.jpeg';
-// import scratchImg from './images/Scratch.png';
-// import mazeImg from './images/maze.jpeg';
-
 function HomePage() {
     return (
         <div class="home">
