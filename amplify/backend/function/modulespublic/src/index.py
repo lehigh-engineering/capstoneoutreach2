38010693e4203f1b4f87222e096d1c605c1328ec
Jacob The Losen <jwt325@lehigh.edu>
2024-09-26 15:10:49 -0400

    provisioned table

--- amplify/backend/function/modulespublic/src/index.py
+++ amplify/backend/function/modulespublic/src/index.py
@@ -1,15 +1,36 @@
-import json
+import boto3
 
-def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+def lambda_handler(event, context):
+    try:
+        # Initialize the DynamoDB client
+        dynamodb = boto3.resource('dynamodb')
+        
+        # Name of your DynamoDB table
+        table_name = 'modules'
+        
+        # Retrieve the DynamoDB table
+        table = dynamodb.Table(table_name)
+        
+        # Get all items from the DynamoDB table
+        response = table.scan()
+        
+        # Extract the items from the response
+        items = response.get('Items', [])
+        
+        # Extract IDs and titles from each module
+        modules_info = [{'id': item['id'], 'title': item['title'], 'level': item['level'], 'keyword': item['keyword'], 'description': item['description'], 'url': item['url']} for item in items]
+        
+        # Return the IDs and titles
+        return {
+            'statusCode': 200,
+            'body': modules_info
+        }
+    except Exception as e:
+        # Log the error for debugging purposes
+        print(f"An error occurred: {e}")
+        
+        # Return an error response
+        return {
+            'statusCode': 500,
+            'body': 'Internal Server Error'
+        }
