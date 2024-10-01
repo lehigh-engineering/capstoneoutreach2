630d9edf4c07eb8c90fd57e3d2d6ac20cee9d0df
Jacob The Losen <jwt325@lehigh.edu>
2024-10-01 14:54:43 -0400

    idk

--- amplify/backend/function/modulespublic/src/index.py
+++ amplify/backend/function/modulespublic/src/index.py
@@ -1,5 +1,23 @@
 import boto3
 import os
+from decimal import Decimal
+import json
+
+def decimal_default(obj):
+    if isinstance(obj, Decimal):
+        return float(obj)
+    raise TypeError
+
+def send_response(statusCode, body):
+    return {
+        'statusCode': statusCode,
+        'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': json.dumps(body, default=decimal_default)
+    }
 
 def handler(event, context):
     try:
@@ -22,16 +40,11 @@ def handler(event, context):
         modules_info = [{'id': item['id'], 'title': item['title'], 'level': item['level'], 'keyword': item['keyword'], 'description': item['description'], 'url': item['url']} for item in items]
         
         # Return the IDs and titles
-        return {
-            'statusCode': 200,
-            'body': modules_info
-        }
+        return send_response(200, modules_info)
+    
     except Exception as e:
         # Log the error for debugging purposes
         print(f"An error occurred: {e}")
         
         # Return an error response
-        return {
-            'statusCode': 500,
-            'body': 'Internal Server Error'
-        }
+        return send_response(500, f"An error occurred: {e}")
