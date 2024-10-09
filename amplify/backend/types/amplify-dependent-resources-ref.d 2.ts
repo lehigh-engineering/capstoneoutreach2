c25d1f28f43e78719fafd6259ddb1ffc52233b6e
ktl325 <ktl325@lehigh.edu>
2024-10-09 14:44:37 -0400

    merged dev into kyra

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d 2.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "capstoneoutreachgateway": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "modulespublic": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "capstonemodules": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    }
+  }
+}
