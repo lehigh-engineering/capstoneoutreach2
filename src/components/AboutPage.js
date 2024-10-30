1c13496c6b4d7208c8a50dfe9dd7b62019f8f420
Lara Flaten <lnf325@lehigh.edu>
2024-10-30 04:10:32 -0400

    Pixelated header font

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -13,7 +13,7 @@ function AboutPage() {
         <div class="about">
             <section class="ourMission">
                 <div class="missionText">
-                    <h2>Our Mission</h2>
+                    <h1>Our Mission</h1>
                     <p>The Computer Science Outreach Program involves collaboration with Lehigh University's P.C. Rossin College of Engineering and its outreach manager, Chayah Wilbers, to develop educational outreach assets aimed at sparking interest in computer science among late elementary, middle school, and high school students.</p>
                     <p>Our goal was to broaden Lehigh's outreach to the wider Bethlehem community, addressing the gap in computer science education and compliance with STEELS standards by 2025.</p>
                     <p>Our proposed solution was the creation of a website hosting computer science lessons with accompanying plans and activities. The lessons were made to be fun, easy to follow, and hopefully inspire students in the Bethlehem area.</p>
@@ -27,13 +27,13 @@ function AboutPage() {
                     <img src={packardImg} alt="Classroom"></img>
                 </div>
                 <div class="missionText">
-                    <h2>Our Journey</h2>
+                    <h1>Our Journey</h1>
                     <p>Our journey began in Spring 2024 with a vision to provide teachers in the Bethlehem community with access to high quality stand-alone computer science modules that they can easily incorporate into their cirriculum.</p>
                     <p>Along the way we faced many challenges in assessing user needs and balancing them with the realistic design and functionalities of our solution. However, by utilizing our personal experience in computer science and adopting a customer first mentality, we published six modules closely aligned with the STEELS Standards to a user friendly site  </p>
                     <p>As we look ahead, we're excited to continue expanding the resources we offer, refining our site and content, and reach beyond the Bethlehem community, offering these resources to teachers and students all over.</p>
                 </div>
             </section>
-            <h1>Our Team</h1>
+            <h2>Our Team</h2>
             <section class="ourTeam">
                 <div class="teamMember">
                   <img src={kyraImg} alt="Kyra"></img> 
